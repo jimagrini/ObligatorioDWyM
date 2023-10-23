e8353991721e5f3f484d13616cba078111ec3cb5
panchei19 <francisco.cabarcos@correo.ucu.edu.uy>
2023-10-23 20:26:45 -0300

    cambios

--- obligatorio/src/app/app-routing.module.ts
+++ obligatorio/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { SelectProposalComponent } from './select-proposal/select-proposal.component';
+import { SelectActivityComponent } from './select-activity/select-activity.component';
+import { CreateProposalComponent } from './create-proposal/create-proposal.component';
+import { CreateActivityComponent } from './create-activity/create-activity.component';
+
+const routes: Routes = [
+  { path: 'select-proposal', component: SelectProposalComponent },
+  { path: 'select-activity', component: SelectActivityComponent },
+  { path: 'create-proposal', component: CreateProposalComponent },
+  { path: 'create-activity', component: CreateActivityComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
