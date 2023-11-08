4afb1a7a83129c49585abd9d654cbe3a7a5215e6
panchei19 <francisco.cabarcos@correo.ucu.edu.uy>
2023-11-08 09:17:27 -0300

    base de datos game

---
+++ src/database/models/gameSchema.js
@@ -0,0 +1,36 @@
+
+const mongoose = require('mongoose');
+const gameSchema = require('./gameSchema');
+
+const proposalSchemaSchema = require('./proposalSchema');
+const adminSchema = require('./adminSchema');
+
+var actvs  = mongoose.model('Proposal', proposalSchemaSchema);
+
+const gameSchema = new mongoose.Schema({
+
+    id:  {
+        type: Number,
+        required: true
+    },
+
+    members:  {
+        type: String,
+        required: true
+    },
+    fromDate:  {
+        type: Date,
+        required: true
+    },
+    upToDate:  {
+        type: Date,
+        required: true
+    },
+
+
+    proposals: [{ type: mongoose.Schema.Types.ObjectId, ref: 'proposalSchema' }],
+    admins: [{ type: mongoose.Schema.Types.ObjectId, ref: 'adminSchema' }]
+
+});
+
+module.exports = mongoose.model('proposal', proposalSchema);
