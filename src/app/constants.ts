d6a1ceedfd547a3dcf2a32fd10716e8e32c8fe9c
Felipe Villaronga <felipe.villaronga@correo.ucu.edu.uy>
2023-11-14 16:50:19 -0300

    refacotrizacion en constants en ACTIVITIES

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -11,16 +11,16 @@ export const ADMINISTRATORS: IAdmin[] = [
 ];
 
 export const ACTIVITIES: IActivity[] = [
-    { id: 1, name: 'Paseo por el bosque', category: 'Onix Premiere', description: 'Onix Premiere', image: new URL("https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg"), selected: false },
-    { id: 2, name: 'Tarde en la playa', category: 'Onix Premiere', description: 'Onix Premiere', image: new URL("https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg"), selected: false },
-    { id: 3, name: 'Campeonato de fifa', category: 'Onix Premiere', description: 'Onix Premiere', image: new URL("https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg"), selected: false },
-    { id: 4, name: 'Torneo de truco', category: 'Onix Premiere', description: 'Onix Premiere', image: new URL("https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg"), selected: false },
-    { id: 5, name: 'Asado', category: 'Onix Premiere', description: 'Onix Premiere', image: new URL("https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg"), selected: false },
-    { id: 6, name: 'Mates en en el parque', category: 'Onix Premiere', description: 'Onix Premiere', image: new URL("https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg"), selected: false },
-    { id: 7, name: 'Ir al club', category: 'Onix Premiere', description: 'Onix Premiere', image: new URL("https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg"), selected: false },
-    { id: 8, name: 'Ir al gimnasio', category: 'Onix Premiere', description: 'Onix Premiere', image: new URL("https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg"), selected: false },
-    { id: 9, name: 'Chevrolet', category: 'Onix Premiere', description: 'Onix Premiere', image: new URL("https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg"), selected: false },
-    { id: 10, name: 'Chevrolet', category: 'Onix Premiere', description: 'Onix Premiere', image: new URL("https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg"), selected: false },
+    { id: 1, name: 'Paseo por el bosque', category: 'Onix Premiere', description: 'Onix Premiere', image: "https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg", selected: false },
+    { id: 2, name: 'Tarde en la playa', category: 'Onix Premiere', description: 'Onix Premiere', image: "https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg", selected: false },
+    { id: 3, name: 'Campeonato de fifa', category: 'Onix Premiere', description: 'Onix Premiere', image: "https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg", selected: false },
+    { id: 4, name: 'Torneo de truco', category: 'Onix Premiere', description: 'Onix Premiere', image: "https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg", selected: false },
+    { id: 5, name: 'Asado', category: 'Onix Premiere', description: 'Onix Premiere', image: "https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg", selected: false },
+    { id: 6, name: 'Mates en en el parque', category: 'Onix Premiere', description: 'Onix Premiere', image: "https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg", selected: false },
+    { id: 7, name: 'Ir al club', category: 'Onix Premiere', description: 'Onix Premiere', image: "https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg", selected: false },
+    { id: 8, name: 'Ir al gimnasio', category: 'Onix Premiere', description: 'Onix Premiere', image: "https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg", selected: false },
+    { id: 9, name: 'Chevrolet', category: 'Onix Premiere', description: 'Onix Premiere', image: "https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg", selected: false },
+    { id: 10, name: 'Chevrolet', category: 'Onix Premiere', description: 'Onix Premiere', image: "https://www.megautos.com/wp-content/uploads/2019/10/chevrolet-onix-premier-2020-dinamica.jpg", selected: false },
 ];
 
 export const CATEGORIES = ['Deportiva', 'Visita', 'Al aire libre', 'Cultural', 'Lúdica', 'Relajación', 'Audio-visual'];
